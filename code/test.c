1de4eed0b25d38ee0c0faff95de35071cd5f6927
Mayer Florian <florian.mayer@fh-joanneum.at>
2023-11-14 16:52:33 +0100

    Template v2.1 corrected reported errors from students

---
+++ code/test.c
@@ -0,0 +1,16 @@
+	#include <stdio.h>
+	#define N 10
+	
+	int main()
+	{
+	int i;
+	
+	puts("Hello world!");
+	
+	for (i = 0; i < N; i++)
+	{
+	puts("LaTeX is also great for programmers!");
+	}
+	
+	return 0;
+	}
